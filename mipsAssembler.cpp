045ab91126c9f27e5cb42a81438b57312d372347
maustin8 <maustin@mtech.edu>
2017-02-25 19:43:50 -0700

    Comments Added
    Descriptions of each method were added.

--- mipsAssembler.cpp
+++ mipsAssembler.cpp
@@ -4,6 +4,9 @@
 //
 //
 
+//-------------
+// C++ Includes
+//-------------
 #include <iostream>
 #include <istream>
 #include <fstream>
@@ -12,48 +15,158 @@
 #include <stdlib.h>
 #include <vector>
 #include <sstream>
-
-//Holds structs
 #include "fileParser.cpp"
 
-//Namespaces
+//-----------------
+// Using Statements
+//-----------------
 using namespace std;
 
-//Data Structures
+//-----------------
+// Data Stuctures
+//-----------------
 struct symbol{ 
-
 	char label[10]; 
 	int32_t address;
 };
+
 typedef struct symbol symbol;
+
+//This vector stores each label and their respective addresses.
+//This is used for branch and jump statements.
 vector<symbol> symbolTable;
 
+//Starting address
 int32_t currAddress = 0x00400000;
 
+/*********************************************************
+* DESCRIPTION - scanLabels
+* Does the first pass of the input data and creates the 
+* symbol table.
+*
+* PARAMETERS
+* string filename: name of the input file
+*
+**********************************************************/
 void scanLabels(string filename);
+
+/*********************************************************
+* DESCRIPTION - assembleLine
+* Performs the second pass of the input data and creates
+* each instruction line by tokenizing each part of the 
+* instruction lines.
+*
+* PARAMETERS
+* string filename: name of the input file
+*
+**********************************************************/
 void assembleLine(string filename);
+
+/*********************************************************
+* DESCRIPTION - makeRtype
+* Creates the machine code for instructions that are in the
+* R-format.
+*
+* PARAMETERS
+* char* opCode: opCode instruction from input file
+* char* r2: 	The rirst register read in from the input line
+* char* r3:		The second register read in from the input line
+* char* r4:		The final register read in from the input line
+* string line:	The entire line from the input file
+*
+**********************************************************/
 void makeRtype(char* opCode,char* r2,char* r3,char* r4,string line);
+
+/*********************************************************
+* DESCRIPTION - makeItype
+* Creates the machine code for instructions that are in the
+* I-format.
+*
+* PARAMETERS
+* char* opCode: opCode instruction from input file
+* char* r2: 	The rirst register read in from the input line
+* char* r3:		The second register read in from the input line
+* char* r4:		The final register read in from the input line
+* string line:	The entire line from the input file
+*
+**********************************************************/
 void makeItype(char* opCode,char* r2,char* r3,char* r4,string line);
+
+/*********************************************************
+* DESCRIPTION - makeJtype
+* Creates the machine code for instructions that are in the
+* J-format.
+*
+* PARAMETERS
+* char* opCode: opCode instruction from input file
+* char* r2: 	The address that will be jumped to
+* string line:	The entire line from the input file
+*
+**********************************************************/
 void makeJtype(char* opCode,char* r2,string line);
+
+/*********************************************************
+* DESCRIPTION - stripReg
+* Strips the $ off of the registers
+*
+* PARAMETERS
+* char* reg: 	register directly from the input file with
+*				the $ still on it
+*
+* RETURNS
+* Returns the register input but with the $ stripped off
+*
+**********************************************************/
 char* stripReg(char* reg);
+
+/*********************************************************
+* DESCRIPTION - printHeader
+* Prints the header of the output up to the start of the 
+* mips instructions.
+*
+**********************************************************/
 void printHeader();
+
+/*********************************************************
+* DESCRIPTION - displayInstruct
+* Prints the mips instruction followed by its respective 
+* address and machine code.
+*
+* PARAMETERS
+* string line:		The full instruction line from the input file
+* int32_t mCode:	The machine code calculated for the instruction
+*
+**********************************************************/
 void displayInstruct(string line, int32_t mCode);
+
+/*********************************************************
+* DESCRIPTION - getLabelAddress
+* Gets the address for a label from the symbol table.
+*
+* PARAMETERS
+* char* label:		The label whose address need to be found
+*					in the symbol table
+*
+* RETURNS
+* The address that the label is located at.
+*
+**********************************************************/
 int getLabelAddress(char* label);
 
-
 int main(){
-	
 	string filename;
-	filename = "test2.txt";//For testing NOT FOR FINAL
+	
+	cout << "Enter name of input file: ";
+	cin >> filename;
+	
 	scanLabels(filename);
 	printHeader();
 	assembleLine(filename);
-
+	
 	return 0;
-}
+}//main()
 
 void scanLabels(string filename){
-	
 	string line;
 	ifstream inputfile(filename.c_str());
 	
@@ -61,23 +174,19 @@ void scanLabels(string filename){
 		//This strips out the .txt line
 		getline(inputfile,line);
 		while(getline(inputfile, line)){
-			// if we see a label (defined as ending in a colon)
+			// If we see a label (defined as ending in a colon)
 			// add it to the symbolTable
 			if(strstr(line.c_str(), ":") != NULL ){
-				
 				symbol newSymbol;
 				char temp[100];
 				strcpy(temp, line.c_str());
-				
 				//Assigns label's name after stripping colon
 				strcpy(newSymbol.label, strtok(temp, ":"));
 				//then assigns the memory location/address of the label
 				newSymbol.address = currAddress;
-				
 				//Push newly created symbol into symbolTable
 				symbolTable.push_back(newSymbol);
-			}
-			
+			}//if
 			currAddress += 4;
 		}//while
 		
@@ -86,7 +195,6 @@ void scanLabels(string filename){
 	}else{
 		cout << "Unable to open file!" << endl;
 	}//if
-	
 }//scanLabels()
 
 void assembleLine(string filename){
@@ -110,43 +218,40 @@ void assembleLine(string filename){
 		cout << line << endl;
 		
 		while(getline(inputfile,line)){
-			
 			size_t pos = line.find(":");
 			string newLine = line.substr(pos+1);
-			
-			
 			pos = newLine.find("#");
+			
 			if(pos != string::npos)
 				newLine = newLine.substr(0,pos);
 			
-			
 			string endChar = newLine.substr(newLine.length()-1);
+			
 			if(endChar != " "){
-				newLine.append("\t\t");
+				newLine.append("\t");
 			}
+			
 			char temp[100];
 			strcpy(temp, newLine.c_str());
-			
-			
-			
 			opCode = strtok(temp, delim);
-	
+			
 			if(opCode != NULL)
 				r2 = strtok(NULL, delim);
 			if(r2 != NULL)
 				r3 = strtok(NULL, delim);
 			if(r3 != NULL)
 				r4 = strtok(NULL, delim);
-	
+			
 			if( strstr(opCode, "i") != NULL || 
 				strstr(opCode, "w") != NULL ||
 				strstr(opCode, "bne")!= NULL ||
 				strstr(opCode, "beq")!= NULL){
-			
+				
 				makeItype(opCode, r2, r3, r4, line);
 			}
 			else if(strstr(opCode, "j") != NULL &&
 					strstr(opCode,"jr") == NULL){
+				
 				makeJtype(opCode, r2,line);
 			}
 			else
@@ -157,22 +262,20 @@ void assembleLine(string filename){
 	}else
 		cout << "Unable to open file!" << endl;
 	inputfile.close();
-		
-		
 }//assembleLine()
 
 void makeRtype(char* opCode,char* r2,char* r3,char* r4,string line){
-	
 	int32_t mCode = 0;
-	
 	int opFunct, rd, rs, rt;
 	
-
 	if(strstr(opCode,"jr") != NULL){
+		string endChar = line.substr(line.length()-1);
+		if(endChar != " "){
+			line.append("\t");
+		}
 		r3 = stripReg(r2);
 		rd = 0x0;
 		rt = 0x0;
-		
 	}
 	else{
 		r2 = stripReg(r2);
@@ -185,12 +288,10 @@ void makeRtype(char* opCode,char* r2,char* r3,char* r4,string line){
 		
 		if(strcmp(opCode,rTypeInstruct[indx].name) == 0)
 			opFunct = rTypeInstruct[indx].opcode;
-		
-		
-	}//for - opCode instructions
-	indx = -1;
+		}//for - opCode instructions
+		indx = -1;
+	
 	while(registerNum[++indx].name != NULL){
-		
 		
 		if(strcmp(r2,registerNum[indx].name) == 0)
 			rd = registerNum[indx].regNum;
@@ -201,7 +302,7 @@ void makeRtype(char* opCode,char* r2,char* r3,char* r4,string line){
 			rt = registerNum[indx].regNum;
 		}
 	}
-
+	
 	mCode = (0<<26);
 	mCode |= rs << 21;
 	mCode |= rt << 16;
@@ -217,9 +318,6 @@ void makeItype(char* opCode,char* r2,char* r3,char* r4,string line){
 	int32_t mCode = 0;
 	int16_t immed = 0;
 	int opFunct, rt, rs;
-	
-
-	
 	if(strstr(opCode,"w") != NULL){
 		char* temp = stripReg(r4);
 		immed = atoi(r3);
@@ -242,20 +340,17 @@ void makeItype(char* opCode,char* r2,char* r3,char* r4,string line){
 	
 	int indx = -1;
 	while(iTypeInstruct[++indx].name != NULL){
-		
 		if(strcmp(opCode,iTypeInstruct[indx].name) == 0)
 			opFunct = iTypeInstruct[indx].opcode;
-		
-	}//for - opCode instructions
+	}
+	
 	indx = -1;
 	while(registerNum[++indx].name != NULL){
-				
 		if(strcmp(r2,registerNum[indx].name) == 0)
 			rt = registerNum[indx].regNum;
 		if(strcmp(r3,registerNum[indx].name) == 0)
 			rs = registerNum[indx].regNum;
-	}//for - register numbers
-	
+	}
 	
 	mCode = immed;
 	mCode &=~ 0xFFFF0000;
@@ -263,28 +358,25 @@ void makeItype(char* opCode,char* r2,char* r3,char* r4,string line){
 	mCode |= rs << 21;
 	mCode |= rt << 16;
 	
-	
-	
 	displayInstruct(line, mCode);
 }//makeItype()
 
 void makeJtype(char* opCode,char* r2,string line){
 	
+	string endChar = line.substr(line.length()-1);
+	if(endChar != " "){
+		line.append("\t");
+	}
 	int32_t mCode = 0;
-	
 	int opFunct;
-
-	
 	int indx = -1;
+	
 	while(jTypeInstruct[++indx].name != NULL){
-		
 		if(strcmp(opCode,jTypeInstruct[indx].name) == 0)
 			opFunct = jTypeInstruct[indx].opcode;
-		
 	}//for - opCode instructions
-	
+
 	int jumpAdd = getLabelAddress(r2);	
-	
 	
 	mCode = jumpAdd;
 	mCode &=~ 0xFF000000;
@@ -293,23 +385,21 @@ void makeJtype(char* opCode,char* r2,string line){
 	displayInstruct(line, mCode);
 }//makeJType();
 
-
-
 char* stripReg(char* reg){
 	char* cut = strtok(reg,"$");
 	return cut;
-}
+}//stripReg()
 
 void printHeader(){
-
 	cout << endl << "Symbol Table:" << endl << endl;
 	cout << "Label\tAddress(in hex)" << endl;
+	
 	for(int indx = 0; indx < symbolTable.size(); indx++){
 		cout << symbolTable.at(indx).label << "\t";
 		printf("%08x\n",symbolTable.at(indx).address);
 	}
 	cout << endl << "MIPS code\t\t\t\t\tAddress     Machine Lang." << endl;
-}//printTable()
+}//printHeader()
 
 void displayInstruct(string line, int32_t mCode){
 	
@@ -322,21 +412,17 @@ void displayInstruct(string line, int32_t mCode){
 		cout << newLine;
 	}
 	else{
-		cout << temp << "\t\t\t\t";
+		cout << temp << "\t\t";
 	}
-	
 	printf("%08x",currAddress);
 	cout << "\t";
 	printf("%08x\n",mCode);
 }//displayInstruct()
 
 int getLabelAddress(char* label){
-	
 	for(int indx = 0; indx < symbolTable.size(); indx++){
 		if(strcmp(symbolTable[indx].label,label) == 0)
 			return symbolTable[indx].address;
 	}
 	return -1;
-}
-
-
+}//getLabelAddress()
